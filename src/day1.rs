e8344dc8421c65c8b586c020789fdba7685208cd
Christopher Czyba <christopher.czyba@gmail.com>
2023-12-01 17:03:32 +0100

    refactor

--- src/day1.rs
+++ src/day1.rs
@@ -8,99 +8,95 @@ fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<impl Iterator<Item 
     Ok(reader.lines().map(|l| l.expect("Could not parse line")))
 }
 
-pub fn task1() {
+const DIGITS : [(&'static str, i32); 10] = [
+    ("0", 0),
+    ("1", 1),
+    ("2", 2),
+    ("3", 3),
+    ("4", 4),
+    ("5", 5),
+    ("6", 6),
+    ("7", 7),
+    ("8", 8),
+    ("9", 9),
+];
+
+const DIGITS_INCLUDING_WRITTEN_OUT_DIGITS : [(&'static str, i32); 19] = [
+            ("0", 0),
+            ("1", 1),
+            ("2", 2),
+            ("3", 3),
+            ("4", 4),
+            ("5", 5),
+            ("6", 6),
+            ("7", 7),
+            ("8", 8),
+            ("9", 9),
+            ("one", 1),
+            ("two", 2),
+            ("three", 3),
+            ("four", 4),
+            ("five", 5),
+            ("six", 6),
+            ("seven", 7),
+            ("eight", 8),
+            ("nine", 9),
+        ];
+
+pub struct InOrderReplacer<'a, T> {
+    input: &'a str,
+    replacements: &'a[(&'a str, T)],
+    current: usize,
+}
+
+impl<'a, T: Clone> Iterator for InOrderReplacer<'a, T> {
+    type Item = T;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.current >= self.input.len() {
+            return None;
+        }
+        for start_index in self.current..self.input.len() {
+            let substring = &self.input[start_index..self.input.len()];
+            for (digit, value) in self.replacements {
+                if substring.starts_with(*digit) {
+                    self.current = start_index + 1;
+                    return Some(value.clone());
+                }
+            }
+        }
+        return None;
+    }
+}
+
+fn replacer<'a, T>(input: &'a str, replacements:  &'a [(&'a str, T)]) -> InOrderReplacer<'a, T> {
+    return InOrderReplacer {
+        input,
+        replacements,
+        current: 0,
+    };
+}
+
+fn calculate_value(replacements:  &[(&str, i32)]) {
     let c = lines_from_file("src/day1_task1.txt").unwrap()
         .map(|line| {
-            line.chars()
-                .filter(|c| { c.is_numeric() } )
-                .fold((-1, -1), |acc, val| {
-                    let value = val.to_digit(10).unwrap() as i32;
-                    if acc.0 == -1 { 
-                        (value, value)
-                    } else {
-                        (acc.0, value)
-                    }
+            replacer(&line, replacements)
+                .fold(None, |acc, val| {
+                    acc.or_else(|| Some((val, val)))
+                        .map(|v| (v.0, val))
                 })
         }).map(|p| {
-            p.0 * 10 + p.1
+            p.map(|(l,r)| l * 10 + r)
+                .unwrap_or(0)
         }).fold(0, i32::wrapping_add);
 
     println!("{}", c);
 }
 
-fn replace_written_digits(input: &str) -> String {
-    let replacements = vec![
-        ("one", "1"),
-        ("two", "2"),
-        ("three", "3"),
-        ("four", "4"),
-        ("five", "5"),
-        ("six", "6"),
-        ("seven", "7"),
-        ("eight", "8"),
-        ("nine", "9"),
-    ];
-
-    let mut result = String::new();
-    let mut skip = 0;
-    for start_index in 0..input.len() {
-        let substring = &input[start_index..input.len()];
-        for &(digit, value) in &replacements {
-            if substring.starts_with(digit) {
-                skip = digit.len();
-                result.push_str(value);
-                break;
-            }
-        }
-        if skip > 0 {
-            skip -= 1;
-        } else {
-            result.push(substring.chars().next().unwrap());
-        }
-    }
-
-    return result;
+pub fn task1() {
+    calculate_value(&DIGITS);
 }
 
 pub fn task2() {
-    let c = lines_from_file("src/day1_task1.txt").unwrap()
-        .map(|line| {
-            replace_written_digits(&line)
-                .chars()
-                .filter(|c| { c.is_numeric() } )
-                .fold((-1, -1), |acc, val| {
-                    let value = val.to_digit(10).unwrap() as i32;
-                    if acc.0 == -1 { 
-                        (value, value)
-                    } else {
-                        (acc.0, value)
-                    }
-                })
-        }).map(|p| {
-            p.0 * 10 + p.1
-        }).fold(0, i32::wrapping_add);
-
-    println!("{}", c);
-}
-
-#[cfg(test)]
-mod test {
-
-    use super::replace_written_digits;
-
-    #[test]
-    pub fn test() {
-        assert_eq!("21", replace_written_digits("twone"));
-        assert_eq!("o9", replace_written_digits("onine"));
-        assert_eq!("18", replace_written_digits("oneight"));
-
-        assert_eq!("219", replace_written_digits("two1nine"));
-        assert_eq!("823", replace_written_digits("eightwothree"));
-        assert_eq!("abc123xyz", replace_written_digits("abcone2threexyz"));
-        assert_eq!("x2134", replace_written_digits("xtwone3four"));
-        assert_eq!("49872", replace_written_digits("4nineeightseven2"));
-        assert_eq!("z18234", replace_written_digits("zoneight234"));
-        assert_eq!("7pqrst6teen", replace_written_digits("7pqrstsixteen"));
-    }
-
+    calculate_value(&DIGITS_INCLUDING_WRITTEN_OUT_DIGITS);
 }
